9587a1459e652266812ecfa0a7416b2f226fe2db
akshat-ithub <akshatg929@gmail.com>
2025-06-29 14:44:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ const swiper = new Swiper(".mySwiper", {
+          loop: true,
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          },
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          }
+        });
